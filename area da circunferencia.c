1da914ba0876b9db11c3f6ddc3cb8c13f4140d81
anajuliacandido <anajuliadomingo078@gmail.com>
2025-09-07 22:47:50 -0300

    Create area da circunferencia.c

---
+++ area da circunferencia.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#define PT 3.14159
+int main()
+{
+    printf("Hello World\n");
+    
+    float raio, area;
+    printf("Digite o raio da circunferencia: "); // aqui a pessoa ira colocar o raio da circunferencia
+    scanf("%f", &raio);
+    area = PT * raio * raio;
+    printf("Area = %f\n", area );
+    printf("MAS ESSA MANOBRA É UMA \n MANOBRA CONCURSO, UM PROFICIONAL NÃO FARIA UMA MERDA DESSA");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
